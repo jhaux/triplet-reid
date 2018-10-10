fix input scaling

triplet_reid/edflow_implementations/data.py
@@ -23,35 +23,11 @@ def add_labels(data, idx):
     file_id = data.labels["file_id"][idx]
     labels["pid"] = "S{:03}P{:03}".format(S, P)
     labels["name"] = file_id
+    # furthermore, keypoints and box are not properly prepared to be stacked,
+    # replace with zero since we do not need them anyway
+    labels["keypoints"] = 0
+    labels["box"] = 0
     return labels
-
-
-########## old testing code
-
-def PretrainReidNTU(config):
-    # base dataset
-    ntu = PretrainDataset(config)
-
-    # center crop
-    ntu = ProcessedDataset(ntu, center_crop)
-
-    # get multiple views of same pid
-    dataset = JoinedDataset(ntu, "pid", config.get("n_views", 4))
-
-    return dataset
-
-
-def QueryGalleryNTU(config):
-    n_examples = 100
-    prng = np.random.RandomState(1)
-    query = PretrainDataset(config)
-    query = SubDataset(query, prng.choice(len(query), n_examples))
-    gallery = PretrainDataset(config)
-    gallery = SubDataset(gallery, prng.choice(len(gallery), n_examples))
-
-    data = ConcatenatedDataset(query, gallery)
-    data = ProcessedDataset(data, center_crop)
-    return data
 
 
 # NTUGems

triplet_reid/edflow_implementations/implementations.py
@@ -64,7 +64,10 @@ class reIdModel(object):
 
         with tf.variable_scope(self.model_name):
             self.images = tf.placeholder(tf.float32, shape=[None, h, w, nc])
-            endpoints, body_prefix = model.endpoints(self.images,
+            # edflow works with images in [-1,1] but reid net expects [0,255]
+            self.rescaled_images = (self.images+1.0)*127.5
+            input_images = self.rescaled_images
+            endpoints, body_prefix = model.endpoints(input_images,
                                                      is_training=is_train,
                                                      prefix=self.model_name + '/')
             with tf.name_scope('head'):
@@ -79,10 +82,12 @@ class reIdModel(object):
 
     @property
     def inputs(self):
+        # (bs, h, w, c) in [-1, 1]
         return {'image': self.images}
 
     @property
     def outputs(self):
+        # (bs, 128
         return {'embeddings': self.embeddings}
 
 
@@ -115,6 +120,7 @@ class Unstack(Hook):
             image = image[...,:3]
         feeds[self.iterator.model.inputs["image"]] = image
 
+        # add pids for training
         pids = batch["pid"]
         bs, n_views = pids.shape
         pids = pids.reshape(bs*n_views)
@@ -262,8 +268,6 @@ def resize(image, w=TRIP_W, h=TRIP_H):
             im = imresize(im, [w, h])
         ims += [im]
     ims = np.array(ims)
-    # edflow works with images in [-1,1] but reid net expects [0,255]
-    ims = (ims+1.0)*127.5
 
     return ims
 